4997f18bf2ae4e177417af4eec0f2761f5084583
SulaimanovU <uliana.sulaimanov@bk.ru>
2022-02-05 18:19:13 +0600

    feat: dal

---
+++ src/api/api.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: "https://61dd7484f60e8f0017668817.mockapi.io/",
+
+})
+
+export default {
+    getAllPizzas: () => api.get("getall/pizza")
+}
--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -1,44 +1,39 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import css from './admin.module.css'
 import { useState } from 'react';
+import axios from "axios"
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
-const Admin = ({ setIsAuth}) => {
+
+const Admin = ({ setIsAuth }) => {
   const [user, setUser] = useState("")
   const [password, setPassword] = useState("")
   const [error, setError] = useState("")
   const [isDisabled, setIsDisabled] = useState(false)
   const submit = (e) => {
     e.preventDefault();
-    // if(authorized){
-    //   return history.push("/dashboard")
-    // }
     setIsDisabled(true)
-    fetch("https://pizza-app-ulan.herokuapp.com/admin", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        login: user,
-        password: password
-      })
+    axios.post("https://pizza-app-ulan.herokuapp.com/admin", {
+      login: user,
+      password: password
     })
       .finally(() => {
         setIsDisabled(false)
       })
-      .then((res) => res.json())
-      .then((data) => {
-        // TODO: remove push after login logic
-        if (data?.token) {
-          setIsAuth(data)
+      .then((res) => {
+        if (res.data?.token) {
+          setIsAuth(res.data)
         } else {
-          setError(data.msg)
+          setError(res.data.msg)
         }
       })
       .catch((error) => {
         console.log(error)
       })
+  }
+  const history = useHistory()
+  const logout = () => {
+    history.push("/")
   }
 
   return (
@@ -46,23 +41,24 @@ const Admin = ({ setIsAuth}) => {
       <form onSubmit={submit} className={css.form} >
         <div className={css.login__content}>
           <div className={css.sm}>
-            <h2 className={css.title}>Добро пожаловать</h2>
-            <p className={css.des}>Редактирование сайта: добавление удаление и корректировка</p>
+            <h2 className={css.title}>Welcome</h2>
+            <p className={css.des}>Edit the site</p>
           </div>
           <div className={css.ig}>
             <label className={css.label}>
-              <span className={css.phonenum}>Введите свой логин</span>
+              <span className={css.phonenum}>Enter your name</span>
               <div className={css.input__container}>
-                <input placeholder='логин' className={css.input} value={user} onChange={(e) => setUser(e.target.value)} />
+                <input placeholder='Name' className={css.input} value={user} onChange={(e) => setUser(e.target.value)} />
               </div>
-              <span className={css.phonenum}>Введите пароль</span>
+              <span className={css.phonenum}>Enter your password</span>
               <div className={css.input__container}>
-                <input placeholder='пароль' className={css.input} type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                <input placeholder='Password' className={css.input} type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
               </div>
             </label>
           </div>
           <div className={css.cm}>
-            <button disabled={isDisabled} className={css.loginbtn}>Подтвердить</button>
+            <button disabled={isDisabled} className={css.loginbtn}>Log In</button>
+            <button className={css.lgo} disabled={isDisabled} onClick={logout}>Log out</button>
           </div>
         </div>
         <div className='error'>{error}</div>
